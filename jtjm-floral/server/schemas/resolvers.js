c2b8d2c9fd2b12e34df137ce8cda3c0aa66f0495
Jenna Jay <jenneh83@gmail.com>
2024-04-18 19:18:57 -0500

    Updated models and resolver mutations

--- jtjm-floral/server/schemas/resolvers.js
+++ jtjm-floral/server/schemas/resolvers.js
@@ -1,16 +1,52 @@
-const { User, Service, userService } = require('../models');
+const { User, Service, Reserve } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         users: async () => {
-            return await User.find({}).populate('services');
+            return await User.find();
         },
         
         services: async () => {
             return await Service.find({}).populate('users');
-        }
-     }
+        },
+
+        reserves: async () => {
+            return await Reserve.find({}).populate('users');
+        },
+     },
+
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addReserve: async (parent, { name, service, description, price }) => {
+            const newReserve = await Reserve.create({ name, service, description, price });
+            return newReserve;
+        },
+
+        removeReserve: async (parent, { reserveId }) => {
+            return await Reserve.findOneAndDelete({ _id: reserveId });
+        },
+    },    
 };
+
+
 
 module.exports = resolvers;
